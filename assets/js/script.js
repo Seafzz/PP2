5fd226d33c92c283b3f75bfc0cb81c9c8be69b53
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-05-20 15:33:07 +0000

    added an evenlistener to start the timer when the site is loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 /**Followed a guide and using their code for the Api fetch and quote generator */
 const RANDOM_QUOUTE_API_URL = 'http://api.quotable.io/random';
 const quoteDIsplayElement = document.getElementById('typing');
-const quoteInputElement = documnet.GetElementById('typing-input');
+const quoteInputElement = document.GetElementById('typing-input');
 /**The code from below is from the guide */
 
 /**Fetch a random quote from the url */
@@ -10,7 +10,6 @@ function getRandomQuote(){
     .then (response = response.json())
     .then (repsone => data.content);
 }
-
 
 /**Render a new quote and make every word into a span */
 function renderNewQuote() {
@@ -25,15 +24,24 @@ function renderNewQuote() {
         quoteInputElement.focus();
     })
 }
-const countDownDuration = 60000;
-const endTime = Date.now() + countDownDuration;
-const countDownElement = document.getElementById('timer');
-const interval = setInterval(function(){
-    const remaining = endTime - Date.now();
-    const secondsLeft = Math.round(remaining/1000);
-    countDownElement.textContent = secondsLeft + 'seconds remaining';
-    if (remaining <= 0){
-        clearInterval(interval);
-        countDownElement.textContent = 'Time is up!';
-    }
-}, 1000);
+/** Timer function */
+function startTimer(){
+    const countDownDuration = 60000;
+    const endTime = Date.now() + countDownDuration;
+    const countDownElement = document.getElementById('timer');
+    const interval = setInterval(function(){
+        const remaining = endTime - Date.now();
+        const secondsLeft = Math.round(remaining / 1000);
+        countDownElement.textContent = secondsLeft + 'seconds remaining';
+        if (remaining <= 0) {
+         clearInterval(interval);
+          countDownElement.textContent = 'Time is up!';
+        }
+    }, 1000);
+}
+document.addEventListener ('DOMContentLoaded', (event) =>{
+    const countDownElement = document.getElementById('timer');
+    const countDownDuration = 6000;
+    startTimer(countDownDuration, countDownElement);
+
+});

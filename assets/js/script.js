e7a91f4a63fe246a3368132d2e31b39ece802e1f
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-05-20 15:22:02 +0000

    added QuoteDisplay and QuoteInput element by Id to the script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 /**Followed a guide and using their code for the Api fetch and quote generator */
 const RANDOM_QUOUTE_API_URL = 'http://api.quotable.io/random';
+const quoteDIsplayElement = document.getElementById('typing');
+const quoteInputElement = documnet.GetElementById('typing-input');
 /**The code from below is from the guide */
 
 /**Fetch a random quote from the url */
 function getRandomQuote(){
-    return fetch(RANDOM_QUOTE_API_URL)
+    return fetch(RANDOM_QUOUTE_API_URL)
     .then (response = response.json())
     .then (repsone => data.content);
 }
@@ -23,15 +25,15 @@ function renderNewQuote() {
         quoteInputElement.focus();
     })
 }
-const countDownDuration = 6000;
+const countDownDuration = 60000;
 const endTime = Date.now() + countDownDuration;
-const countDonwElement = document.getElementById('timer');
+const countDownElement = document.getElementById('timer');
 const interval = setInterval(function(){
     const remaining = endTime - Date.now();
     const secondsLeft = Math.round(remaining/1000);
-    countDownELement.textContent = secondsLeft + 'seconds remaining';
+    countDownElement.textContent = secondsLeft + 'seconds remaining';
     if (remaining <= 0){
         clearInterval(interval);
-        countDownELement.textContent = 'Time is up!';
+        countDownElement.textContent = 'Time is up!';
     }
 }, 1000);

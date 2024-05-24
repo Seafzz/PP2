264f903ad5452202e6947268585c8f0e3345098a
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-05-24 13:11:47 +0000

    fixed yet again another error in the selector where I didnt target the class errors span because of a missing .

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('typing')
 const quoteInputElement = document.getElementById('typing-input')
-const errorsElement = document.querySelector('errors span');
+const errorsElement = document.querySelector('.errors span');
 const wpmElement = document.querySelector('.cpm span');
 const cpmElement = document.querySelector('.wpm span');
 let gameRunning = true;
